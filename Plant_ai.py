a98c142c69233f40a8406bf46951cf243aff7044
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-08-21 19:37:50 +0700

    FEAT: add feature ai assistant

--- Plant_ai.py
+++ Plant_ai.py
@@ -12,6 +12,20 @@ import torchvision.transforms as transforms
 from PIL import Image
 import os
 import sys
+import speech_recognition as sr  # recognise speech
+import playsound  # to play an audio file
+from gtts import gTTS  # google text to speech
+import random
+from time import ctime  # get time details
+import webbrowser  # open browser
+import threading
+
+class person:
+    name = 'Mr'
+
+    def setName(self, name):
+        self.name = name
+
 
 class Plant_Disease_Model(nn.Module):
     def __init__(self):
@@ -25,18 +39,23 @@ class Plant_Disease_Model(nn.Module):
         return out
 
 class ShowImage(QMainWindow):
+    voice_signal = QtCore.pyqtSignal(str, str)  # Define the signal
     def __init__(self):
         super(ShowImage, self).__init__()
         loadUi('GUI.ui', self)
         self.image = None
         self.cap = None
+        self.recording = False
+        self.voice_thread = None
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.pushButton_4.clicked.connect(self.loadClicked)
         self.pushButton_5.clicked.connect(self.predictFromImage)
         self.pushButton_2.clicked.connect(self.stopCam)
         self.pushButton.clicked.connect(self.startCam)
+        self.pushButton_6.clicked.connect(self.toggleVoiceAi)
         self.pushButton_3.clicked.connect(self.reset)
+        self.voice_signal.connect(self.update_text_edit)
 
     def displayImage(self, image, label):
         qformat = QImage.Format_Indexed8
@@ -78,6 +97,89 @@ class ShowImage(QMainWindow):
                 print("Failed to read image")
         else:
             print("No file selected")
+    
+    def update_text_edit(self, user_text, assistant_text):
+        self.textEdit_2.append(f"You: {user_text}")
+        self.textEdit_2.append(f"Assistant: {assistant_text}")
+    
+    def toggleVoiceAi(self):
+        if self.recording:
+            self.stopVoiceAi()
+        else:
+            self.startVoiceAi()
+
+    def startVoiceAi(self):
+        self.recording = True
+        self.pushButton_6.setText("Stop Recording")
+        self.voice_thread = threading.Thread(target=self.record_voice)
+        self.voice_thread.start()
+
+    def stopVoiceAi(self):
+        self.recording = False
+        self.pushButton_6.setText("Start Recording")
+        if self.voice_thread:
+            self.voice_thread.join()  # Wait for the thread to finish
+
+    def record_voice(self):
+        while self.recording:
+            voice_data = record_audio()
+            if voice_data:
+                response = self.generate_response(voice_data)
+                self.voice_signal.emit(voice_data, response)
+                speak(response)
+
+    def generate_response(self, voice_data):
+        response = ""  # Initialize response with an empty string
+
+        if there_exists(['hey', 'hi', 'hello'], voice_data):
+            greetings = [
+                f"Hey {person_obj.name}, how can I help you?",
+                f"Hey, what's up {person_obj.name}?",
+                f"I'm listening {person_obj.name}",
+                f"How can I assist you? {person_obj.name}",
+                f"Hello {person_obj.name}"
+            ]
+            response = greetings[random.randint(0, len(greetings) - 1)]
+        
+        elif there_exists(["what is your name", "what's your name", "tell me your name"], voice_data):
+            if person_obj.name:
+                response = f"My name is PlantAI, {person_obj.name}."
+            else:
+                response = "My name is PlantAI. What's your name?"
+        
+        elif there_exists(["my name is", "i am"], voice_data):
+            person_name = voice_data.split("is")[-1].strip()
+            response = f"Okay, I will remember that {person_name}."
+            person_obj.setName(person_name)
+        
+        elif there_exists(["what is alternaria solani", "alernaria solani"], voice_data):
+            response = "Alternaria solani is a fungus that causes early blight in tomatoes and potatoes. It is known for producing dark, concentric lesions on the leaves and stems of these plants."
+        
+        elif there_exists(["who are you"], voice_data):
+            response = "I am your assistant in the field of plants. I am PlantAI."
+        elif there_exists(["Tell me about tomato blight"], voice_data):
+            response = "Tomato blight is a serious disease that affects tomato plants. It is caused by a fungus called Phytophthora infestans. This disease can cause the leaves to turn brown and wither, and in severe cases, it can kill the plant."
+        elif there_exists(["describe seed plants"], voice_data):
+            response = "a group of plants that have seeds as a means of reproduction. Seeds are the result of fertilization between egg cells and pollen, which function as a means of spreading and maintaining the survival of plant species."
+        elif there_exists(["thanks", "thank you", "thank u"], voice_data):
+            response = "You're welcome! If you need anything else, just let me know."
+        elif there_exists(["open webcam"], voice_data):
+            self.cap = cv2.VideoCapture(0)
+            self.timer.start(20) 
+            response = "Webcam has been opened."
+        elif there_exists(["close webcam"], voice_data):
+            self.timer.stop()
+            if self.cap is not None:
+                self.cap.release()
+            self.label.clear()  # Clear the label content
+            self.label.setText("Camera Off")  # Set default text
+            response = "Webcam has been closed"
+        
+        else:
+            response = "I'm not sure how to respond to that."
+
+        return response
+        
 
     def startCam(self):
         self.cap = cv2.VideoCapture(0)
@@ -98,6 +200,7 @@ class ShowImage(QMainWindow):
         self.label_2.setText("No Image")
         self.label_7.clear()
         self.textEdit.clear()
+    
 
     def update_frame(self): 
         ret, frame = self.cap.read()
@@ -107,7 +210,6 @@ class ShowImage(QMainWindow):
             cv2.putText(frame, prediction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             # Display the frame with the prediction text
             self.displayImage(frame, self.label)
-            self.label_2.clear() 
 
     def predictFromImage(self):
         if self.image is not None:
@@ -119,6 +221,59 @@ class ShowImage(QMainWindow):
         else:
             self.label_2.setText("No image loaded.")
             self.label_6.clear()
+
+person_obj = person()
+
+def there_exists(terms, voice_data):
+    for term in terms:
+        if term in voice_data:
+            return True
+    return False
+
+
+def record_audio(ask=False):
+    r = sr.Recognizer()  # initialise a recogniser
+    with sr.Microphone() as source:  # microphone as source
+        print("Say something...")
+        if ask:
+            speak(ask)
+        r.adjust_for_ambient_noise(source)
+        try:
+            audio = r.listen(source)  # listen for the audio with a timeout  
+        except sr.WaitTimeoutError:
+            speak('Sorry, I did not hear anything. Please try again.')
+            return ""
+        print("Recognizing now...")
+        
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError:  # error: recognizer does not understand
+            speak('I did not get that')
+            print("Recognition failed: UnknownValueError")
+        except sr.RequestError:
+            speak('Sorry, the service is down')  # error: recognizer is not connected
+            print("Recognition failed: RequestError")
+        
+        if voice_data:
+            print(f">> {voice_data.lower()}")  # print what user said
+        else:
+            print("No voice data captured")
+
+        return voice_data.lower()
+
+
+def speak(audio_string):
+    if not audio_string:
+        print("No text to speak")
+        return
+    tts = gTTS(text=audio_string, lang='en')  # text to speech(voice)
+    r = random.randint(1, 20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file)  # save as mp3
+    playsound.playsound(audio_file)  # play the audio file
+    print(f"PlantAI: {audio_string}")  # print what app said
+    os.remove(audio_file)  # remove the audio file after playback
 
 
 transform = transforms.Compose([
